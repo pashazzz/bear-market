3b1a6bb1ae49fe196e924acddf0cb1bea2223181
Pavlo Malyshkin <pmalyshkin@gmail.com>
2023-12-20 13:28:01 +0200

    adding simple Input element

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+
+import './Input.css'
+
+interface Props {
+  type?: string
+  title?: string
+  placeholder?: string
+  value: string | number
+  setValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Input: React.FunctionComponent<Props> = (props: Props) => {
+  return (
+    <div className='input-wrapper'>
+      { props.title && <div className='input-title'>{props.title}</div> }
+      <input
+        type={props.type ?? 'text'}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={(e) => props.setValue(e.target.value) }
+      />
+    </div>
+  )
+}
+
+export default Input
