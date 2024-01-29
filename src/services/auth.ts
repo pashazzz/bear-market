61e7d96cc06e27c98210e8f9bf69ab8474a6da71
Pavlo Malyshkin <pmalyshkin@gmail.com>
2024-01-29 12:28:14 +0200

    small cleanup

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,6 @@
 import { getRequestWithAuth } from "../helpers/backendRequsts"
 
 // check is user have role and type
-// types 'athlete' and 'organizer' have not roles
-// type 'staff' has roles and can imitate athletes and organizers
-// by having role 'athlete' and 'organizer'
 export const hasRole = async (role: string) => {
   const user = JSON.parse(localStorage.getItem('user') ?? '{}')
 
